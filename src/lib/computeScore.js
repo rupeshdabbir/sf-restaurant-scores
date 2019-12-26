afc8c55434ab9077cbd14431057b2543b20c25c4
Rupesh Dabbir <d.rupeshkumar@gmail.com>
2019-12-26 18:42:08 +0530

    Risk Score

--- src/lib/computeScore.js
+++ src/lib/computeScore.js
@@ -6,6 +6,26 @@ const risk = {
 	'HIGH': 'High Risk'
 }
 
+
+/*
+ * Compute score takes an account 2 parameters while coming up with a score:
+ *  A) "Inspection Score"  - This is the score officially given by sfgov/
+ *  B) "Risk Category" - This identifies what kind of risk does the inspection pose.
+
+ * Note: sfgov has given same scores for "Medium" and "low" risk factors.
+ * Hence, the aim of my "Confidence Score" is to provide an accurate confidence value based on these two
+ * Parameters in the big picture.
+
+ * Algorithm:
+   - If risk category is "LOW": 
+      Score    :  GRADE
+   	- <90-100> - A
+   	- <80-99>  - A-
+   	- 80 and below: B
+
+   	and so on for other risk categories.
+*/
+
 export function computeScore(score, category) {
 	if(score === 0 || category === 'unspecified') return 'F';
 
@@ -14,16 +34,16 @@ export function computeScore(score, category) {
 	switch (category) {
 		case risk.LOW:
 			return computeLowRiskScore(score);
-			// break;
 		case risk.MED:
 			return computeMediumRiskScore(score);
-			// break;
 		case risk.HIGH:
 			return computeHighRiskScore(score);
-			// break;
 	}
 }
 
+
+
+/* Helper Methods */
 function computeLowRiskScore(score) {
 	if(isHighScore(score)) return 'A';
 	if(isModerateScore(score)) return 'A-';
